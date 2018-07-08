1ac83560c5269507d5b0b809ad44798bce0b36b5
Derek Croote <derek.croote@gmail.com>
2018-07-08 13:12:10 -0700

    Add gap removal to sequence filling
    In sequences that have deletions, IgBLAST will report gaps, which can
    cause trouble for biopython sequence translation.

--- btreceptor/sequences.py
+++ btreceptor/sequences.py
@@ -20,11 +20,11 @@ def _vfill(v_ref, seq):
     """ Replace any nucleotides missing from the start of the V gene
     """
 
-    v_ref = v_ref[:25]
-    v_seq = seq[:25]
+    v_ref = v_ref[:75]
+    v_seq = seq[:75]
 
     v_filled = _fill_missing_nt(v_ref, v_seq)
-    seq_filled = v_filled + seq[25:]
+    seq_filled = v_filled + seq[75:]
 
     return seq_filled
 
@@ -45,7 +45,7 @@ def global_pw_align(s0, s1):
 
 
 def _fill_missing_nt(ref_seq, seq):
-    """ Perform pairwise alignment between two sequences and fills in
+    """ Perform pairwise alignment between two sequences and fill in
         missing nucleotides
     """
 
@@ -64,12 +64,17 @@ def _fill_missing_nt(ref_seq, seq):
     return filled
 
 
-def fill_sequence(seq_id, v_ref_seq, j_ref_seq, seq):
-    """ Fill in missing nucleotide at the beginning of the
-        V gene or end of the J gene
+def fill_clean_sequence(seq_id, v_ref_seq, j_ref_seq, seq):
+    """ Clean gaps arising from deletions and fill in missing nucleotides at
+        the beginning of the V gene or end of the J gene.
     """
 
-    vfilled = _vfill(v_ref_seq, seq)
+    if '-' in seq:
+        degap = seq.replace('-','')
+    else:
+        degap = seq
+
+    vfilled = _vfill(v_ref_seq, degap)
 
     vjfilled = _jfill(j_ref_seq, vfilled)
 
--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -3,7 +3,7 @@ from btreceptor import sequences
 
 def test_fill_missing_nt():
     ref_seq = 'ATATGGGGCCTCTAC'
-    test_seq =  'TATGGGGCCCTAC'
+    test_seq =  'TATGGGGCCCT'
 
     filled = sequences._fill_missing_nt(ref_seq, test_seq)
 
@@ -26,3 +26,18 @@ def test_j_fill():
     filled = sequences._jfill(ref_seq, seq)
 
     assert filled == seq + 'CGG'
+
+
+def test_fill_clean():
+    seq_id = 'test_with_gap'
+    ref_v = 'ATATATGCGCGCATGCATGTCGCATAGCGCGACGTCTA'
+    ref_j = 'ATTTCGCTGCAACGGATCTGAACCGGCCTTCACACATT'
+    fake_cdr3 = 'ATGCAGATGGGAACCGGG'
+
+    # missing 1 nucleotide in beginning and end, has gap in V
+    seq = ref_v[1:10] + '---' + ref_v[10:-5] + fake_cdr3 + ref_j[5:-1]
+
+    expected = ref_v[:-5] + fake_cdr3 + ref_j[5:]
+    outcome = sequences.fill_clean_sequence(seq_id, ref_v, ref_j, seq)
+
+    assert outcome == expected
